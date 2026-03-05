bbe67d0cbdabefe3e1984387f716c61ec57557f1
christof.schaffer@student.htl-rankweil.at <christof.schaffer@student.htl-rankweil.at>
2026-03-05 11:13:35 +0000

    Edited Impact_Detail

--- client_code/Impact_Detail/__init__.py
+++ client_code/Impact_Detail/__init__.py
@@ -15,26 +15,38 @@ class Impact_Detail(Impact_DetailTemplate):
     self.item = anvil.server.call("get_impact_details", impact_id)
     self.headline.text = self.item["einschlag_name"]
 
-    self.init_impact_plot()
+    self.init_bomb_card()
+    self.init_impact_card()
     
   @handle("back_button", "click")
   def back_button_click(self, **event_args):
     open_form('Home')
     pass
 
+  ####
+  #### ------ bomb
+  ####
+  
   def init_bomb_card(self):
     self.init_bomb_text()
-    self.init_impact_plot()
     
   def init_bomb_text(self):
-    pass
-  
+    self.bomb_headline.text = self.item['waffen_name']
+    self.bomb_label.text = f"""Explosion Power:{self.item['waffen_leistung']}
+    Weapon-Type: {self.item['waffen_typ']}"""
+    print(self.item)
+  ####
+  #### ------ impact
+  ####
+  def init_impact_card(self):
+    self.init_impact_plot()
+    
   def init_impact_plot(self):
     # Daten für das Diagramm vorbereiten
     labels = ['Todesopfer', 'Verletzte']
     values = [self.item['todesopfer'], self.item['verletzte']]
 
-    self.bomb_plot.data = [
+    self.impact_plot.data = [
       go.Pie(
         labels=labels,
         values=values,
@@ -45,9 +57,30 @@ class Impact_Detail(Impact_DetailTemplate):
     ]
 
     # Layout anpassen (Titel, Farben, etc.)
-    self.bomb_plot.layout = {
-      'title': f"Opferstatistik: {self.item['waffe_name']}",
+    self.impact_plot.layout = {
       'paper_bgcolor': 'rgba(0,0,0,0)', # Transparent an den Hintergrund anpassen
       'plot_bgcolor': 'rgba(0,0,0,0)',
-      'font': {'color': '#333'}
+      'font': {'color': '#333'},
+      'margin': {'l': 0, 'r': 0, 't': 0, 'b': 0},
+      'autosize': True,
     }
+
+  def init_impact_map(self):
+    self.impact_map.map_data.add(GoogleMap.Data.Feature(
+      geometry=GoogleMap.Data.Point(
+        GoogleMap.LatLng(self.item['latitude'],self.item['longitude']))))
+
+    self.impact_map.map_data.style = GoogleMap.Data.StyleOptions(
+      icon=GoogleMap.Symbol(
+        path=GoogleMap.SymbolPath.CIRCLE,
+        scale=30,
+        fill_color='red',
+        fill_opacity=0.3,
+        stroke_opacity=1,
+        stroke_weight=1
+      )
+    )
+
+  ####
+  #### ------ impact
+  ####
